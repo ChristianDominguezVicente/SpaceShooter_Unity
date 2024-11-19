db4c84e526e626b5639e7834cd6301b869165a7a
ChristianDominguezVicente <chris_dovi@usal.es>
2024-11-19 14:36:31 +0100

    19/11/2024

--- Assets/Scripts/Disparo.cs
+++ Assets/Scripts/Disparo.cs
@@ -1,21 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 using UnityEngine.Windows;
 
 public class Disparo : MonoBehaviour
 {
     [SerializeField] private float velocidad;
     [SerializeField] private Vector3 direccion;
+
+    private ObjectPool<Disparo> mypool;
+
+    public ObjectPool<Disparo> MyPool { get => mypool; set => mypool = value; }
+
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Destroy(this.gameObject, 4);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(direccion * velocidad * Time.deltaTime);
+
+        timer += Time.deltaTime;
+
+        if (timer >= 4)
+        {
+            timer = 0;
+            mypool.Release(this);
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float velocidad;
     [SerializeField] private float ratioDisparo;
-    [SerializeField] private GameObject disparoPrefab;
-    [SerializeField] private GameObject spawnPoint1;
-    [SerializeField] private GameObject spawnPoint2;
+    [SerializeField] private Disparo disparoPrefab;
+    [SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private int numeroDisparos;
     private float temporizador = 0.5f;
     private float vidas = 100;
+
+    private ObjectPool<Disparo> pool;
+    private void Awake()
+    {
+        pool = new ObjectPool<Disparo>(CrearDisparo, null, ReleaseDisparo, DestroyDisparo);
+    }
+
+    private Disparo CrearDisparo()
+    {
+        Disparo disparoCopia = Instantiate(disparoPrefab, transform.position, Quaternion.identity);
+        disparoCopia.MyPool = pool;
+        return disparoCopia;
+    }
+
+    private void ReleaseDisparo(Disparo disparo)
+    {
+        disparo.gameObject.SetActive(false);
+    }
+    private void DestroyDisparo(Disparo disparo)
+    {
+        Destroy(disparo.gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -44,8 +69,27 @@ public class Player : MonoBehaviour
         temporizador += 1 * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space) && temporizador > ratioDisparo)
         {
-            Instantiate(disparoPrefab, spawnPoint1.transform.position, Quaternion.identity);
-            Instantiate(disparoPrefab, spawnPoint2.transform.position, Quaternion.identity);
+            /* // poner en una corrutina para que haya efectos, este es espiral
+            float gradosPorDisparo = 360 / numeroDisparos;
+            
+            for (float i = 0; i < 360; i+=gradosPorDisparo)
+            {
+                // Estoy pidiendo a la piscina que me de un nuevo disparo.
+                Disparo disparocopia = pool.Get();
+                disparocopia.gameObject.SetActive(true);
+                disparocopia.transform.position = spawnPoints[i].transform.position;
+                disparocopia.transform.eulerAngles =new Vector3(0f, 0f, i);
+                yield return new WaitforSeconds(0.2f);
+            }
+            */
+            for (int i = 0; i < 2; i++)
+            {
+                // Estoy pidiendo a la piscina que me de un nuevo disparo.
+                Disparo disparocopia = pool.Get();
+                disparocopia.gameObject.SetActive(true);
+                disparocopia.transform.position = spawnPoints[i].transform.position;
+                
+            }
             temporizador = 0;
         }
     }
